ca9368ddc57e2afe2b670de18cfbe65b8b508905
Fox Scarlett <foxontheone@gmail.com>
2025-11-09 00:50:09 -0800

    initial commit

---
+++ build/fox-duplex-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd9ae05f0b8a002d2180f');
---
+++ build/fox-duplex-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c4cef04dd2d1b2695ed8');
